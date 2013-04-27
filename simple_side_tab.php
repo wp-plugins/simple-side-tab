97f04ae67498b2348cf15a4b35c0f2f3a86c0ee1
srumery <srumery@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-27 16:48:58 +0000

    Publish v1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-side-tab/trunk@704726 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- simple_side_tab.php
+++ simple_side_tab.php
@@ -1,29 +1,29 @@
 <?php
 /*
 Plugin Name: Simple Side Tab
-Plugin URI: http://www.rumspeed.com/plugins/simple-side-tab
+Plugin URI: http://rumspeed.com/wordpress-plugins/simple-side-tab/
 Description: Display a side tab that you can easily link to any page. Customize the tab text, font and colors. It's that simple. That's Simple Side Tab.
-Version: 0.8.6
+Version: 1.0.0
 Author: Scot Rumery
 Author URI: http://rumspeed.com/scot-rumery/
 License: GPLv2
 */
 
-/*  Copyright 2012  Scot Rumery (email : scot@rumspeed.com)
+/*  Copyright 2013  Scot Rumery (email : scot@rumspeed.com)
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 
@@ -46,7 +46,8 @@ function rum_sst_activate_plugin() {
 		'pixels_from_top'  => '350',
 		'text_color'       => '#FFFFFF',
 		'tab_color'        => '#A0244E',
-		'hover_color'      => '#A4A4A4'
+		'hover_color'      => '#A4A4A4',
+		'target_blank'     => '0'
 		);
 
 	// create field in WP_options to store all plugin data in one field
@@ -65,26 +66,57 @@ if ( is_admin() ){ // admin actions and filters
 	add_action( 'admin_init', 'rum_sst_settings_api_init');
 
 	// Hook to fire farbtastic includes for using built in WordPress color picker functionality
-	add_action('init', 'rum_sst_farbtastic_script');
+	add_action('admin_enqueue_scripts', 'rum_sst_farbtastic_script');
 
 	// Display the 'Settings' link in the plugin row on the installed plugins list page
 	add_filter( 'plugin_action_links_'.plugin_basename(__FILE__), 'rum_sst_admin_plugin_actions', -10);
 
 } else { // non-admin enqueues, actions, and filters
 
-	// hook to get option values and dynamically render css to support the tab classes
-	add_action( 'wp_head', 'rum_sst_custom_css_hook' );
 
-	// hook to get option values and write the div for the Simple Side Tab to display
-	add_action( 'wp_footer', 'rum_sst_body_tag_html' );
+	// get the current page url
+	$rum_current_page_url 			= rum_get_full_url();
 
+
+	// get the tab url from the plugin option variable array
+	$rum_sst_plugin_option_array	= get_option( 'rum_sst_plugin_options' );
+	$rum_sst_tab_url				= $rum_sst_plugin_option_array[ 'tab_url' ];
+
+
+	// compare the page url and the option tab - don't render the tab if the values are the same
+	if ( $rum_sst_tab_url != $rum_current_page_url ) {
+
+		// hook to get option values and dynamically render css to support the tab classes
+		add_action( 'wp_head', 'rum_sst_custom_css_hook' );
+
+		// hook to get option values and write the div for the Simple Side Tab to display
+		add_action( 'wp_footer', 'rum_sst_body_tag_html' );
+	}
+}
+
+
+
+// get the complete url for the current page
+function rum_get_full_url()
+{
+	$s 			= empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+	$sp 		= strtolower($_SERVER["SERVER_PROTOCOL"]);
+	$protocol 	= substr($sp, 0, strpos($sp, "/")) . $s;
+	$port 		= ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
+	return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
 }
 
 
 
 // Include WordPress color picker functionality
-function rum_sst_farbtastic_script() {
+function rum_sst_farbtastic_script($hook) {
 
+	// only enqueue farbtastic on the plugin settings page
+	if( $hook != 'settings_page_rum_simple_side_tab' ) 
+		return;
+
+
+	// load the style and script for farbtastic
 	wp_enqueue_style( 'farbtastic' );
 	wp_enqueue_script( 'farbtastic' );
 
@@ -101,10 +133,15 @@ function rum_sst_body_tag_html() {
 	// fetch individual values from the plugin option variable array
 	$rum_sst_text_for_tab			= $rum_sst_plugin_option_array[ 'text_for_tab' ];
 	$rum_sst_tab_url				= $rum_sst_plugin_option_array[ 'tab_url' ];
+	$rum_sst_target_blank			= $rum_sst_plugin_option_array[ 'target_blank' ];
+
+	// set the page target
+	if ($rum_sst_target_blank == '1') {
+		$rum_sst_target_blank = ' target="_blank"';
+	}
 
 	// Write HTML to render tab
-	echo '<a href="' . $rum_sst_tab_url . '"><div id="rum_sst_tab" class="rum_sst_contents rum_sst_left">' . $rum_sst_text_for_tab . '</div></a>';
-
+	echo '<a href="' . esc_url( $rum_sst_tab_url ) . '"' . $rum_sst_target_blank . '><div id="rum_sst_tab" class="rum_sst_contents rum_sst_left">' . esc_html( $rum_sst_text_for_tab ) . '</div></a>';
 }
 
 
@@ -120,12 +157,6 @@ function rum_sst_admin_menu() {
 // Display and fill the form fields for the plugin admin page
 function rum_sst_options_page() {
 
-	// Validate user has permission to execute this page
-	if ( !current_user_can( 'manage_options' ) )  {
-
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-
-	}
 
 ?>
 
@@ -154,6 +185,7 @@ function rum_sst_options_page() {
 	$rum_sst_text_color				= $rum_sst_plugin_option_array[ 'text_color' ];
 	$rum_sst_tab_color				= $rum_sst_plugin_option_array[ 'tab_color' ];
 	$rum_sst_hover_color			= $rum_sst_plugin_option_array[ 'hover_color' ];
+	$rum_sst_target_blank			= $rum_sst_plugin_option_array[ 'target_blank' ];
 
 ?>
 
@@ -161,114 +193,120 @@ function rum_sst_options_page() {
 
 	<script type="text/javascript">
 
-	  jQuery(document).ready(function() {
-		jQuery('#colorpicker1').hide();
-		jQuery('#colorpicker1').farbtastic("#color1");
-		jQuery("#color1").click(function(){jQuery('#colorpicker1').slideToggle()});
-	  });
+		jQuery(document).ready(function() {
+			jQuery('#colorpicker1').hide();
+			jQuery('#colorpicker1').farbtastic("#color1");
+			jQuery("#color1").click(function(){jQuery('#colorpicker1').slideToggle()});
+		});
 
-	  jQuery(document).ready(function() {
-		jQuery('#colorpicker2').hide();
-		jQuery('#colorpicker2').farbtastic("#color2");
-		jQuery("#color2").click(function(){jQuery('#colorpicker2').slideToggle()});
-	  });
+		jQuery(document).ready(function() {
+			jQuery('#colorpicker2').hide();
+			jQuery('#colorpicker2').farbtastic("#color2");
+			jQuery("#color2").click(function(){jQuery('#colorpicker2').slideToggle()});
+		});
 
-	  jQuery(document).ready(function() {
-		jQuery('#colorpicker3').hide();
-		jQuery('#colorpicker3').farbtastic("#color3");
-		jQuery("#color3").click(function(){jQuery('#colorpicker3').slideToggle()});
-	  });
+		jQuery(document).ready(function() {
+			jQuery('#colorpicker3').hide();
+			jQuery('#colorpicker3').farbtastic("#color3");
+			jQuery("#color3").click(function(){jQuery('#colorpicker3').slideToggle()});
+		});
 
 	</script>
 
 
-    <table class="widefat">
+	<table class="widefat">
 
-        <tr valign="top">
-        <th scope="row" width="230"><label for="rum_sst_text_for_tab">Text for tab</label></th>
-        <td width="525"><input maxlength="30" size="25" type="text" name="rum_sst_plugin_options[text_for_tab]" value="<?php echo $rum_sst_text_for_tab; ?>" /></td>
-        </tr>
+		<tr valign="top">
+		<th scope="row" width="230"><label for="rum_sst_text_for_tab">Text for tab</label></th>
+		<td width="525"><input maxlength="30" size="25" type="text" name="rum_sst_plugin_options[text_for_tab]" value="<?php echo esc_html( $rum_sst_text_for_tab ); ?>" /></td>
+		</tr>
 
 
-        <tr valign="top">
-        <th scope="row"><label for="rum_sst_tab_font">Select font</label></th>
-        <td>
+		<tr valign="top">
+		<th scope="row"><label for="rum_sst_tab_font">Select font</label></th>
+		<td>
 			<select name="rum_sst_plugin_options[font_family]">
-			  <option <?php if ( $rum_sst_font_family =='Arial, sans-serif' )                                   { echo 'selected="selected" '; } ?>value="Arial, sans-serif">ARIAL</option>
-			  <option <?php if ( $rum_sst_font_family =='Georgia, serif' )                                      { echo 'selected="selected" '; } ?>value="Georgia, serif">GEORGIA</option>
-			  <option <?php if ( $rum_sst_font_family =='"Helvetica Neue", Helvetica, sans-serif' )             { echo 'selected="selected" '; } ?>value='"Helvetica Neue", Helvetica, sans-serif'>HELVETICA NEUE / HELVETICA</option>
-			  <option <?php if ( $rum_sst_font_family =='"Lucida Sans Unicode", "Lucida Grande", sans-serif' )  { echo 'selected="selected" '; } ?>value='"Lucida Sans Unicode", "Lucida Grande", sans-serif'>LUCIDA</option>
-			  <option <?php if ( $rum_sst_font_family =='Tahoma, sans-serif' )                                  { echo 'selected="selected" '; } ?>value="Tahoma, sans-serif">TAHOMA</option>
-			  <option <?php if ( $rum_sst_font_family =='"Trebuchet MS", sans-serif' )                          { echo 'selected="selected" '; } ?>value='"Trebuchet MS", sans-serif'>TREBUCHET MS</option>
-			  <option <?php if ( $rum_sst_font_family =='Verdana, sans-serif' )                                 { echo 'selected="selected" '; } ?>value="Verdana, sans-serif">VERDANA</option>
+				<option value="Arial, sans-serif"									<?php selected( $rum_sst_font_family, 'Arial, sans-serif' );									?>	>ARIAL</option>
+				<option value="Georgia, serif"										<?php selected( $rum_sst_font_family, 'Georgia, serif' );										?>	>GEORGIA</option>
+				<option value='"Helvetica Neue", Helvetica, sans-serif'				<?php selected( $rum_sst_font_family, '"Helvetica Neue", Helvetica, sans-serif' );				?>	>HELVETICA NEUE / HELVETICA</option>
+				<option value='"Lucida Sans Unicode", "Lucida Grande", sans-serif'	<?php selected( $rum_sst_font_family, '"Lucida Sans Unicode", "Lucida Grande", sans-serif' );	?>	>LUCIDA</option>
+				<option value="Tahoma, sans-serif"									<?php selected( $rum_sst_font_family, 'Tahoma, sans-serif' );									?>	>TAHOMA</option>
+				<option value='"Trebuchet MS", sans-serif'							<?php selected( $rum_sst_font_family, '"Trebuchet MS", sans-serif' );							?>	>TREBUCHET MS</option>
+				<option value="Verdana, sans-serif"									<?php selected( $rum_sst_font_family, 'Verdana, sans-serif' );									?>	>VERDANA</option>
 			</select>
-        </td>
-        </tr>
+		</td>
+		</tr>
 
 
-        <tr valign="top">
-        <th scope="row"><label for="rum_sst_font_weight_bold">Bold text</label></th>
-        <td><input name="rum_sst_plugin_options[font_weight_bold]" type="checkbox" value="1" <?php checked( '1', $rum_sst_font_weight_bold ); ?> /></td>
-        </tr>
+		<tr valign="top">
+		<th scope="row"><label for="rum_sst_font_weight_bold">Bold text</label></th>
+		<td><input name="rum_sst_plugin_options[font_weight_bold]" type="checkbox" value="1" <?php checked( '1', $rum_sst_font_weight_bold ); ?> /></td>
+		</tr>
 
 
-        <tr valign="top">
-        <th scope="row"><label for="rum_sst_text_shadow">Text shadow</label></th>
-        <td><input name="rum_sst_plugin_options[text_shadow]" type="checkbox" value="1" <?php checked( '1', $rum_sst_text_shadow ); ?> /></td>
-        </tr>
+		<tr valign="top">
+		<th scope="row"><label for="rum_sst_text_shadow">Drop shadow on hover</label></th>
+		<td><input name="rum_sst_plugin_options[text_shadow]" type="checkbox" value="1" <?php checked( '1', $rum_sst_text_shadow ); ?> /></td>
+		</tr>
 
 
-        <tr valign="top">
-        <th scope="row"><label for="rum_sst_tab_url">Tab URL</label></th>
-        <td><input size="45" type="text" name="rum_sst_plugin_options[tab_url]" value="<?php echo $rum_sst_tab_url; ?>" /></td>
-        </tr>
+		<tr valign="top">
+		<th scope="row"><label for="rum_sst_target_blank">Open link in new window</label></th>
+		<td><input name="rum_sst_plugin_options[target_blank]" type="checkbox" value="1" <?php checked( '1', $rum_sst_target_blank ); ?> /></td>
+		</tr>
 
 
-        <tr valign="top">
-        <th scope="row"><label for="rum_sst_pixels_from_top">Position from top (px)</label></th>
-        <td><input maxlength="4" size="4" type="text" name="rum_sst_plugin_options[pixels_from_top]" value="<?php echo $rum_sst_pixels_from_top; ?>" /></td>
-        </tr>
+		<tr valign="top">
+		<th scope="row"><label for="rum_sst_tab_url">Tab URL</label></th>
+		<td><input size="45" type="text" name="rum_sst_plugin_options[tab_url]" value="<?php echo esc_url( $rum_sst_tab_url ); ?>" /></td>
+		</tr>
 
-    </table>
+
+		<tr valign="top">
+		<th scope="row"><label for="rum_sst_pixels_from_top">Position from top (px)</label></th>
+		<td><input maxlength="4" size="4" type="text" name="rum_sst_plugin_options[pixels_from_top]" value="<?php echo sanitize_text_field( $rum_sst_pixels_from_top ); ?>" /></td>
+		</tr>
+
+	</table>
 
 <BR>
 
-    <table class="widefat" border="1">
+	<table class="widefat" border="1">
 
-        <tr valign="top">
+		<tr valign="top">
 			<th scope="row" colspan="2" width="33%"><strong>Colors:</strong> Click on each field to display the color picker. Click again to close it.</th>
 			<td width="33%" rowspan="4">
 				<div id="colorpicker1"></div>
 				<div id="colorpicker2"></div>
 				<div id="colorpicker3"></div>
 			</td>
-        </tr>
+		</tr>
 
 
 		<tr valign="top">
 			<th scope="row"><label for="rum_sst_text_color">Text color</label></th>
-			<td width="33%"><input type="text" maxlength="7" size="6" value="<?php echo $rum_sst_text_color; ?>" name="rum_sst_plugin_options[text_color]" id="color1" /></td>
+			<td width="33%"><input type="text" maxlength="7" size="6" value="<?php echo esc_attr( $rum_sst_text_color ); ?>" name="rum_sst_plugin_options[text_color]" id="color1" /></td>
 		</tr>
 
 
 		<tr valign="top">
 			<th scope="row"><label for="rum_sst_tab_color">Tab color</label></th>
-			<td width="33%"><input type="text" maxlength="7" size="6" value="<?php echo $rum_sst_tab_color; ?>" name="rum_sst_plugin_options[tab_color]" id="color2" /></td>
+			<td width="33%"><input type="text" maxlength="7" size="6" value="<?php echo esc_attr( $rum_sst_tab_color ); ?>" name="rum_sst_plugin_options[tab_color]" id="color2" /></td>
 		</tr>
 
 
 		<tr valign="top">
 			<th scope="row"><label for="rum_sst_hover_color">Tab hover color</label></th>
-			<td width="33%"><input type="text" maxlength="7" size="6" value="<?php echo $rum_sst_hover_color; ?>" name="rum_sst_plugin_options[hover_color]" id="color3" /></td>
+			<td width="33%"><input type="text" maxlength="7" size="6" value="<?php echo esc_attr( $rum_sst_hover_color ); ?>" name="rum_sst_plugin_options[hover_color]" id="color3" /></td>
 		</tr>
 
 		<tr valign="top">
 			<td colspan="3">&nbsp;</td>
 		</tr>
 
-    </table>
+	</table>
 
-    <p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>
+	<p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>
 
 
 
@@ -388,8 +426,5 @@ function rum_sst_custom_css_hook() {
 
 </style>
 
-
-
 <?php
 }
-?>
